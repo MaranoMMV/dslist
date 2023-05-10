0517108af2018281627bfa3604bb5614c9285836
MaranoMMV <matheusmmv14@gmail.com>
2023-05-10 17:28:32 -0300

    Projection Repository and consult SQL

---
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslist.projections;
+
+public interface GameMinProjection {
+
+	Long getId();
+	String getTitle();
+	Integer getYear();
+	String getImgUrl();
+	String getShortDescription();
+	Integer getPosition();
+	
+	
+}
