2100b191ed66f1c9067aa214d08437ac71435b31
MaranoMMV <matheusmmv14@gmail.com>
2023-05-10 16:46:26 -0300

    GET Lists

---
+++ src/main/java/com/devsuperior/dslist/dto/GameListDTO.java
@@ -0,0 +1,37 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.GameList;
+
+public class GameListDTO {
+
+	private Long id;
+	private String name;
+	
+	public GameListDTO() {
+	}
+
+	public GameListDTO(GameList entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+}
